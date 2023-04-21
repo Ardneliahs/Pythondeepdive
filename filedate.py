9bf3599b3d096b9c13b9da71c2f64528e7b2a2a3
Ardneliahs <shailendragautam349@gmail.com>
2023-04-21 19:33:45 +0530

    modify

--- filedate.py
+++ filedate.py
@@ -4,7 +4,7 @@ now_epoch = int(time.time())
 is_modified = False
 for each_file in os.listdir(filedir):
     file_path = os.path.join(filedir,each_file)
-    if os.path.isfile(os.path.join(filedir,each_file)):
+    if os.path.isfile(file_path):
         filemtime = os.path.getctime(file_path)
         if now_epoch - filemtime > 14400:
             is_modified = True
