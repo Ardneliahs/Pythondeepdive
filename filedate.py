e6d8d3f7bdf0e551c179179bdf7c32f7588cd215
Ardneliahs <shailendragautam349@gmail.com>
2023-04-21 19:31:21 +0530

    modify scrupt

--- filedate.py
+++ filedate.py
@@ -1,9 +1,11 @@
 import os, time
 filedir = "/root/Pythondeepdive"
 now_epoch = int(time.time())
+is_modified = False
 for each_file in os.listdir(filedir):
     file_path = os.path.join(filedir,each_file)
-    if os.path.isfile(file_path):
+    if os.path.isfile(os.path.join(filedir,each_file)):
         filemtime = os.path.getctime(file_path)
         if now_epoch - filemtime > 14400:
-            print(file_path)
+            is_modified = True
+print(is_modified)
