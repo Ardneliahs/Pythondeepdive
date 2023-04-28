ae113ad6302ab60a0a4f8bb3b123279392a12468
Ardneliahs <shailendragautam349@gmail.com>
2023-04-28 14:08:29 -0400

    fix

--- write_csv.py
+++ write_csv.py
@@ -1,5 +1,5 @@
 import csv
 fo = open("/root/Pythondeepdive/my.csv","a",newline="")
 csv_writer = csv.writer(fo,delimiter=",")
-csv_writer.writerow("Master of Puppers",1986)
+csv_writer.writerow(["Master of Puppers",1986])
 fo.close()
