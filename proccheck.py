dbc6476924b772c76c10cd959c54c761f87a533d
Ardneliahs <shailendragautam349@gmail.com>
2023-04-28 11:30:59 +0530

    proccheck subprocess

---
+++ proccheck.py
@@ -0,0 +1,15 @@
+import psutil, time, subprocess, sys
+try:
+    named_id = int(subprocess.check_output('pidof named', shell=True))
+except:
+    print('UNKNOWN [script error]')
+    sys.exit(3)
+named_proc = psutil.Process(named_id)
+named_start = int(named_proc.create_time())
+now_epoch = int(time.time())
+if now_epoch - named_start > 1800:
+    print("CRITICAL")
+    sys.exit(2)
+else:
+    print("OK")
+    sys.exit(0)
