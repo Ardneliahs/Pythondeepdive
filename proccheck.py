d3a5300ec67009021e694c52892c820c87a71051
Ardneliahs <shailendragautam349@gmail.com>
2023-04-28 11:37:11 +0530

    more addn

--- proccheck.py
+++ proccheck.py
@@ -4,9 +4,11 @@ try:
     sp = subprocess.Popen('pidof named',shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     rc= sp.wait()
     named_id,named_err = sp.communicate()
+    if rc != 0:
+        print(named_err)
+        sys.exit(4)
 except:
-#    print('UNKNOWN [script error]')
-    print(named_err)
+    print('UNKNOWN [script error]')
     sys.exit(3)
 named_proc = psutil.Process(named_id)
 named_start = int(named_proc.create_time())
