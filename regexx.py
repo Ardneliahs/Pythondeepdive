4c72ce4fc8242c64e93938909fc26ca8e54ef16a
Ardneliahs <shailendragautam349@gmail.com>
2023-05-01 23:19:50 +0530

    ip addr

---
+++ regexx.py
@@ -0,0 +1,5 @@
+import re
+re_pattern = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\"
+re_text = "10.9.68.111"
+print(re.findall(re_pattern,re_text))
+print(len(re.findall(re_pattern,re_text))) # number of occurences
