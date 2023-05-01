e435e3c3536772c14cf09e82475ce9c25b65d484
Ardneliahs <shailendragautam349@gmail.com>
2023-05-01 17:56:05 +0530

    typo

--- imported.py
+++ imported.py
@@ -4,5 +4,5 @@ def sum(a,b):
 def main():
     print(sum(3,2))
 
-if __name__ == "__main":
+if __name__ == "__main__":
     main()
--- importing.py
+++ importing.py
@@ -3,5 +3,5 @@ import imported
 def main():
     print(imported.sum(4,5))
 
-if __name__ == "__main":
+if __name__ == "__main__":
     main()
