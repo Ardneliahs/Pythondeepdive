9f255e3e31db379d462a01315555fef1416c2bea
Ardneliahs <shailendragautam349@gmail.com>
2023-05-21 22:08:03 +0530

    typo

--- param_ssh.py
+++ param_ssh.py
@@ -1,5 +1,5 @@
 import paramiko
-sshc = paramiko.SSHClient()
+ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname='192.168.1.6',username='root',password='Skelter1',port=22)
 stdin, stdout, stderr = ssh.exec_command('uptime')
