310f3c9efe777d8a3e4447f4cb270309b0db88e1
Ardneliahs <shailendragautam349@gmail.com>
2023-05-22 09:32:08 -0400

    added class var:

---
+++ class_var.py
@@ -0,0 +1,17 @@
+class Employee:
+  count=0
+  def __init__(self):
+    Employee.count = Employee.count + 1
+    return None
+
+
+def main():
+  emp1 = Employee()
+  emp2 = Employee()
+  print(Employee.count)
+  print(emp1.count)
+  print(emp2.count)
+
+
+if __name__=="__main__":
+  main()
